654f673975c0797ccde0428190e4a4d9b3a4b3f5
HuimaHeimo <46885479+HuimaHeimo@users.noreply.github.com>
2019-04-05 07:48:44 +0300

    Main version

---
+++ mokkisofta/Paaikkuna.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace mokkisofta
+{
+    partial class Paaikkuna
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Paaikkuna));
+            this.btnTpisteet = new System.Windows.Forms.Button();
+            this.btnPalvelut = new System.Windows.Forms.Button();
+            this.btnVaraukset = new System.Windows.Forms.Button();
+            this.btnAsiakkaat = new System.Windows.Forms.Button();
+            this.btnLaskut = new System.Windows.Forms.Button();
+            this.btnRaportit = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tiedostoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ohjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.poistuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tietojaOhjelmastaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTpisteet
+            // 
+            this.btnTpisteet.Font = new System.Drawing.Font("Copperplate Gothic Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTpisteet.Location = new System.Drawing.Point(12, 64);
+            this.btnTpisteet.Name = "btnTpisteet";
+            this.btnTpisteet.Size = new System.Drawing.Size(167, 60);
+            this.btnTpisteet.TabIndex = 0;
+            this.btnTpisteet.Text = "Toimipisteet";
+            this.btnTpisteet.UseVisualStyleBackColor = true;
+            // 
+            // btnPalvelut
+            // 
+            this.btnPalvelut.Font = new System.Drawing.Font("Copperplate Gothic Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPalvelut.Location = new System.Drawing.Point(12, 130);
+            this.btnPalvelut.Name = "btnPalvelut";
+            this.btnPalvelut.Size = new System.Drawing.Size(167, 60);
+            this.btnPalvelut.TabIndex = 1;
+            this.btnPalvelut.Text = "Palvelut";
+            this.btnPalvelut.UseVisualStyleBackColor = true;
+            // 
+            // btnVaraukset
+            // 
+            this.btnVaraukset.Font = new System.Drawing.Font("Copperplate Gothic Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVaraukset.Location = new System.Drawing.Point(12, 196);
+            this.btnVaraukset.Name = "btnVaraukset";
+            this.btnVaraukset.Size = new System.Drawing.Size(167, 60);
+            this.btnVaraukset.TabIndex = 2;
+            this.btnVaraukset.Text = "Varaukset";
+            this.btnVaraukset.UseVisualStyleBackColor = true;
+            // 
+            // btnAsiakkaat
+            // 
+            this.btnAsiakkaat.Font = new System.Drawing.Font("Copperplate Gothic Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAsiakkaat.Location = new System.Drawing.Point(12, 262);
+            this.btnAsiakkaat.Name = "btnAsiakkaat";
+            this.btnAsiakkaat.Size = new System.Drawing.Size(167, 60);
+            this.btnAsiakkaat.TabIndex = 3;
+            this.btnAsiakkaat.Text = "Asiakkaat";
+            this.btnAsiakkaat.UseVisualStyleBackColor = true;
+            // 
+            // btnLaskut
+            // 
+            this.btnLaskut.Font = new System.Drawing.Font("Copperplate Gothic Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLaskut.Location = new System.Drawing.Point(12, 328);
+            this.btnLaskut.Name = "btnLaskut";
+            this.btnLaskut.Size = new System.Drawing.Size(167, 60);
+            this.btnLaskut.TabIndex = 4;
+            this.btnLaskut.Text = "Laskut";
+            this.btnLaskut.UseVisualStyleBackColor = true;
+            // 
+            // btnRaportit
+            // 
+            this.btnRaportit.Font = new System.Drawing.Font("Copperplate Gothic Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRaportit.Location = new System.Drawing.Point(12, 394);
+            this.btnRaportit.Name = "btnRaportit";
+            this.btnRaportit.Size = new System.Drawing.Size(167, 60);
+            this.btnRaportit.TabIndex = 5;
+            this.btnRaportit.Text = "Raportit";
+            this.btnRaportit.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tiedostoToolStripMenuItem,
+            this.ohjeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(681, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tiedostoToolStripMenuItem
+            // 
+            this.tiedostoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.poistuToolStripMenuItem});
+            this.tiedostoToolStripMenuItem.Name = "tiedostoToolStripMenuItem";
+            this.tiedostoToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.tiedostoToolStripMenuItem.Text = "Tiedosto";
+            // 
+            // ohjeToolStripMenuItem
+            // 
+            this.ohjeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tietojaOhjelmastaToolStripMenuItem});
+            this.ohjeToolStripMenuItem.Name = "ohjeToolStripMenuItem";
+            this.ohjeToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.ohjeToolStripMenuItem.Text = "Ohje";
+            // 
+            // poistuToolStripMenuItem
+            // 
+            this.poistuToolStripMenuItem.Name = "poistuToolStripMenuItem";
+            this.poistuToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.poistuToolStripMenuItem.Text = "Poistu";
+            // 
+            // tietojaOhjelmastaToolStripMenuItem
+            // 
+            this.tietojaOhjelmastaToolStripMenuItem.Name = "tietojaOhjelmastaToolStripMenuItem";
+            this.tietojaOhjelmastaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tietojaOhjelmastaToolStripMenuItem.Text = "Tietoja ohjelmasta";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "https://d1zpvjny0s6omk.cloudfront.net/media/cache/61/2c/612cc1b4a1876c3db01283ed3" +
+    "f52116b.jpg";
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(185, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(484, 390);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Paaikkuna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(681, 509);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnRaportit);
+            this.Controls.Add(this.btnLaskut);
+            this.Controls.Add(this.btnAsiakkaat);
+            this.Controls.Add(this.btnVaraukset);
+            this.Controls.Add(this.btnPalvelut);
+            this.Controls.Add(this.btnTpisteet);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Paaikkuna";
+            this.Text = "Päävalikko";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTpisteet;
+        private System.Windows.Forms.Button btnPalvelut;
+        private System.Windows.Forms.Button btnVaraukset;
+        private System.Windows.Forms.Button btnAsiakkaat;
+        private System.Windows.Forms.Button btnLaskut;
+        private System.Windows.Forms.Button btnRaportit;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tiedostoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem poistuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ohjeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tietojaOhjelmastaToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
